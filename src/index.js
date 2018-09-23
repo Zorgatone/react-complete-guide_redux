851108e91c031b45c02b1c0557cb3a0b4a5805c7
Tommaso Ricci <zorgatone@gmail.com>
2018-09-23 18:02:46 +0200

    Create store and connect state to props

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
 import App from "./App";
+import reducer from "./store/reducer";
 import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import "./index.css";
+
+const store = createStore(reducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
+
 registerServiceWorker();
